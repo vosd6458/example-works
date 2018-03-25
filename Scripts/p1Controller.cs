fa2cfab638728530baa66cf26970d25869ba6d1d
vosd6458 <vosd6458@gmail.com>
2018-03-25 14:19:14 -0500

    Add files via upload

---
+++ Scripts/p1Controller.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class p1Controller : MonoBehaviour {
+
+	//movement variables
+	public float moveSpeed;
+	public float jumpForce;
+	public bool lookingRight;
+
+	//Rigid Body call
+	private Rigidbody2D theRB;
+
+	//movementkeys
+	public KeyCode left;
+	public KeyCode right;
+	public KeyCode jump;
+	public KeyCode shoot;
+
+	//Key Variables
+	public bool lastKeyPressedLeft = false;
+	public bool lastKeyPressedRight = false;
+
+	//Shooting Stuff
+	public GameObject projectilePrefab;
+	GameObject projectileInstance;
+	public float posOffset;
+
+	//groundstuff
+	public Transform groundCheckPointBottom;
+	public Transform groundCheckPointLeft;
+	public Transform groundCheckPointRight;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+
+	//Player Health
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider p1HealthSlider;
+
+	//groundCheck
+	public bool onGround;
+
+	//cooldown vars
+	float baseCooldown = 1f;
+	public float trueCooldown;
+	float nextFire;
+
+	//getting stats
+	private GameObject player1Stats;
+	p1StatsCalc p1Stats;
+
+
+	void Awake(){
+		player1Stats = GameObject.Find ("P1StatSceneManager");
+		p1Stats = player1Stats.GetComponent <p1StatsCalc> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		theRB = GetComponent<Rigidbody2D> ();
+		currentHealth = startingHealth;
+		calculateCooldown ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+		getShoot ();
+		if (currentHealth <= 0) {
+			DestroyObject (gameObject);
+		}
+	}
+
+
+	void Movement (){
+		if (Physics2D.OverlapCircle (groundCheckPointBottom.position, groundCheckRadius, whatIsGround) || Physics2D.OverlapCircle (groundCheckPointLeft.position, groundCheckRadius, whatIsGround) || Physics2D.OverlapCircle (groundCheckPointRight.position, groundCheckRadius, whatIsGround)) {
+			onGround = true;
+		} else {
+			onGround = false;
+		}
+		if (Input.GetKey (left)) {
+			theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+			lastKeyPressedLeft = true;
+			lastKeyPressedRight = false;
+		}
+		else if (Input.GetKey (right)){
+			theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+			lastKeyPressedLeft = false;
+			lastKeyPressedRight = true;
+		} else {
+			theRB.velocity = new Vector2(0, theRB.velocity.y);
+		}
+
+		if (Input.GetKeyDown (jump) && onGround) {
+			theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+		}
+	}
+
+
+	void getShoot(){
+		if (Input.GetKeyDown (shoot) && Time.time > nextFire) {
+			if (lastKeyPressedRight == true) {
+				Vector3 projPosition = new Vector3 (gameObject.transform.position.x + posOffset, gameObject.transform.position.y, gameObject.transform.position.z);
+				projectileInstance = Instantiate (projectilePrefab, projPosition, Quaternion.identity) as GameObject;
+				nextFire = Time.time + trueCooldown;
+			} else {
+				Vector3 projPosition = new Vector3 (gameObject.transform.position.x - posOffset, gameObject.transform.position.y, gameObject.transform.position.z);
+				projectileInstance = Instantiate (projectilePrefab, projPosition, Quaternion.Euler (Vector3.down * 180f)) as GameObject;
+				nextFire = Time.time + trueCooldown;
+			}
+		}
+	}
+
+
+	public void damaged(int amount){
+		if (currentHealth <= 0) {
+			Destroy (gameObject);
+		} else {
+			currentHealth = currentHealth - amount;
+			p1HealthSlider.value = currentHealth;
+		}
+
+	}
+
+	void calculateCooldown(){
+		trueCooldown = baseCooldown + (p1Stats.cooldown / 5f);
+	}
+
+
+}
---
+++ Scripts/p1StatsCalc.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class p1StatsCalc : MonoBehaviour {
+	//Allowed Stat points
+	public int statPoints = 5;
+
+	//player1
+	public int size, damage, speed, cooldown = 0;
+	int fire = 0; //Val taken from UI element
+	int water = 0; //Val taken from UI element
+	int air = 0; //Val taken from UI element
+	int earth = 0; //Val taken from UI element
+
+	//UI text showing Value
+	public Text fireStatNumber;
+	public Text waterStatNumber;
+	public Text airStatNumber;
+	public Text earthStatNumber;
+
+	//UI text showing Damage Size speed CD
+	public Text damageStatNumber;
+	public Text sizeStatNumber;
+	public Text speedStatNumber;
+	public Text cooldownStatNumber;
+
+	//UI text showing Max Stat Points
+	public Text statPointNumber;
+
+	//Stat Points Spent Bool
+	public bool allStatPointsSpent = false;
+
+	//Bools to check if number increases or decreases
+
+
+	//don't destroy object
+
+	//checking Scene
+	public string menu;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this);
+	}	
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		fireStatNumber.text = fire.ToString ();
+		waterStatNumber.text = water.ToString ();
+		airStatNumber.text = air.ToString ();
+		earthStatNumber.text = earth.ToString ();
+
+		damageStatNumber.text = damage.ToString ();
+		sizeStatNumber.text = size.ToString ();
+		speedStatNumber.text = speed.ToString ();
+		cooldownStatNumber.text = cooldown.ToString ();
+
+		statPointNumber.text = statPoints.ToString ();
+		checkPoints ();
+	}
+
+	public void checkPoints(){
+		if (statPoints == 0) {
+			allStatPointsSpent = true;
+		} else {
+			allStatPointsSpent = false;
+		}
+	}
+
+	public void increaseFire(){
+		if (statPoints > 0) {
+			fire++;
+			statPoints--;
+			Debug.Log ("fire increased to: " + fire);
+			size = size - 3;
+			damage = damage + 3;
+			speed = speed + 1;
+			cooldown = cooldown + 2;
+		}
+	}
+	public void increaseWater(){
+		if (statPoints > 0) {
+			water++;
+			statPoints--;
+			Debug.Log ("water increased to: " + water);
+			size = size + 3;
+			damage = damage - 3;
+			speed = speed + 2;
+			cooldown = cooldown + 1;
+		}
+	}
+	public void increaseAir(){
+		if (statPoints > 0) {
+			air++;
+			statPoints--;
+			Debug.Log ("air increased to: " + air);
+			size = size + 2;
+			damage = damage + 1;
+			speed = speed + 3;
+			cooldown = cooldown - 3;
+		}
+	}
+	public void increaseEarth(){
+		if (statPoints > 0) {
+			earth++;
+			statPoints--;
+			Debug.Log ("earth increased to: " + earth);
+			size = size + 1;
+			damage = damage + 2;
+			speed = speed - 3;
+			cooldown = cooldown + 3;
+		}
+	}
+
+	public void decreaseFire(){
+		if (fire == 0) {
+			Debug.Log ("can't decrease Fire already 0");
+		} else {
+			fire--;
+			statPoints++;
+			Debug.Log ("fire decreased to: " + fire);
+			size = size + 3;
+			damage = damage - 3;
+			speed = speed - 1;
+			cooldown = cooldown - 2;
+		}
+
+	}
+	public void decreaseWater(){
+		if (water == 0) {
+			Debug.Log ("can't decrease Water already 0");
+		} else {
+			water--;
+			statPoints++;
+			Debug.Log ("water decreased to: " + water);
+			size = size - 3;
+			damage = damage + 3;
+			speed = speed - 2;
+			cooldown = cooldown - 1;
+		}
+	}
+	public void decreaseAir(){
+		if (air == 0) {
+			Debug.Log ("can't decrease Air already 0");
+		} else {
+			air--;
+			statPoints++;
+			Debug.Log ("air decreased to: " + air);
+			size = size - 2;
+			damage = damage - 1;
+			speed = speed - 3;
+			cooldown = cooldown + 3;
+		}
+	}
+	public void decreaseEarth(){
+		if (earth == 0) {
+			Debug.Log ("can't decrease Earth already 0");
+		} else {
+			earth--;
+			statPoints++;
+			Debug.Log ("earth decreased to: " + earth);
+			size = size - 1;
+			damage = damage - 2;
+			speed = speed + 3;
+			cooldown = cooldown - 3;
+		}
+	}
+
+	public void printStatsChange(){
+		Debug.Log ("Size: " + size + " Damage: " + damage + " Speed: " + speed + " Cooldown: " + cooldown);
+	}
+
+	public void nextSceneP2(){
+		if (allStatPointsSpent == true) {
+			SceneManager.LoadScene ("statPageP2");
+		} else {
+
+		}
+	}
+
+}
